dceaef6b496fa807d800a7571dc134a66f994141
an1serg <anissereg@gmail.com>
2023-09-15 16:47:22 +0300

    Completed Readme and fixed server .env file

--- server/config/database.js
+++ server/config/database.js
@@ -1,9 +1,16 @@
 const { Sequelize } = require('sequelize');
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
-module.exports = new Sequelize('datatable', 'postgres', 'maximbet', {
-  host: 'localhost',
-  dialect: 'postgres',
-  define: {
-    timestamps: false,
-  },
-});
+module.exports = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USERNAME,
+  process.env.DATABASE_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'postgres',
+    define: {
+      timestamps: false,
+    },
+  }
+);
